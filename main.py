a3c7a1607ccbf8f91c736e366e2ce50ddfa50790
Elesential <nick.lowe@dubaiaerospace.com>
2022-12-14 12:07:20 +0000

    Adding in Cause and result data and removing null values from Catagory

--- main.py
+++ main.py
@@ -5,10 +5,12 @@ import numpy as np
 import datetime as dt
 import pycountry
 import plotly.express as px
+import matplotlib
 
-#import Data
+#import Data sets
 data = pd.read_csv(r"C:\Users\nlowe\Downloads\aviation_accidents in countries - aviation_accidents.csv")
-num_accidents = data.shape[0] # saving the count of total accidents
+# saving the count of total accidents
+num_accidents = data.shape[0]
 data.head()
 
 print(data)
@@ -18,6 +20,9 @@ nulls_count_precentage={ col_name : (data[col_name].isna().sum(),
                                 str( round( 100 * data[col_name].isna().sum() / data.shape[0] ,2))+" %")
                                 for col_name in data.columns}
 print(nulls_count_precentage)
+
+#create a bar chart of the number of percentrage of null values
+
 
 #How many unique values are there within the data
 data.nunique()
@@ -32,7 +37,7 @@ def get_country_code(country):
         return result[0].alpha_3
 iso_map = {country: get_country_code(country) for country in data["Country"].unique()}
 
-#Reformating the dataframe keeping details of only the top 15 country and combine all other countries in 'other'
+# ammending the Dataframe - top 15 country and combine all other countries in 'other'
 data["country_code"] = data["Country"].map(iso_map)
 
 countries_df = data.groupby(['Country','country_code']).size().sort_values(ascending=[False])\
@@ -68,3 +73,67 @@ fig = px.treemap(countries_operators_df, path=[px.Constant('world'), 'Country', 
 fig.update_traces(root_color="lightgrey")
 fig.show()
 
+# Fixing the category column and data
+# Some null values exist in the in the category column,
+# Drop Null values in the category column
+categories_filtered=data.dropna(subset=['category'])
+
+# Creating 2 functions to split Cause and Result data and label, adding 2 columns about the accident cause and result
+def cause_accident(value):
+    if value.find('A')>=0:
+        return 'Accident'
+    if value.find('I')>=0:
+        return 'Incident'
+    if value.find('H')>=0:
+        return 'Hijacking'
+    if value.find('C')>=0:
+        return 'Criminal occurrence'
+    if value.find('O')>=0:
+        return 'Other occurrence'
+    if value.find('U')>=0:
+        return 'Unknown'
+
+def result_accident(value):
+    if value.find('1')>=0:
+        return 'Hull Loss'
+    if value.find('2')>=0:
+        return 'Repairable Damage'
+
+
+## Adding 2 columns representing the cause and result of the data individually
+causes = categories_filtered['category'].map(cause_accident)
+results = categories_filtered['category'].map(result_accident)
+categories_filtered = pd.concat([categories_filtered, causes, results], axis=1, join="inner")
+categories_filtered.columns = ['Country', 'date', 'Air-craft type', 'registration name/mark',
+       'operator', 'fatilites', 'location', 'category', 'country_code',
+       'Accident_cause', 'Accident_result']
+
+# Finding the pair of result-cause combination
+result_cause = categories_filtered.groupby(
+                                ['Accident_cause','Accident_result'])  \
+                                .size().sort_values(ascending=False).  \
+                                to_frame().reset_index().  \
+                                rename(columns={0: 'count_accidents'})
+
+# the accident where the result = hull loss
+hull_loss_df = result_cause[result_cause['Accident_result']=='Hull Loss']
+# the accident where the result = Repairable Damage
+repairable_df = result_cause[result_cause['Accident_result']=='Repairable Damage']
+#
+result_by_cause_df = pd.merge(hull_loss_df, repairable_df, how='outer', on = 'Accident_cause').fillna(0)
+result_by_cause_df[:2]
+
+# Create a list for visualisation
+list_draw= [result_by_cause_df["count_accidents_x"].to_list()\
+         ,result_by_cause_df["count_accidents_y"].to_list()]
+
+print(list_draw)
+
+fig = px.imshow(list_draw,
+                labels=dict(x="Cause of Accident", y="Result of Accident", color="Accidents"),
+                x= result_by_cause_df["Accident_cause"],
+                y=["Hull Loss","Repairable Damage"],
+                text_auto = True
+               )
+fig.update_xaxes(side="top" )
+fig.show()
